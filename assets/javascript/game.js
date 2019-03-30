f0ad077db38526ae43be018227f46148802bdf7a
toobaditstaken <alexabbott10@yahoo.com>
2019-03-29 20:46:11 -0500

    have the gems now changing their value after the game resets

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -27,35 +27,53 @@
     $("#number-to-guess").text(targetNumber);
     counter = 0;
     //imageCrystal.attr("data-crystalvalue", giveNumber());
+    $("#crystals").empty();
+    for (var i = 0; i < crystalImages.length; i++) {
     
+        var imageCrystal = $("<img>");
+
+        imageCrystal.addClass("crystal-image");
     
+        
+        imageCrystal.attr("src",crystalImages[i]);
+        imageCrystal.attr("name", "idontcare");
+        
+        
+        imageCrystal.attr("data-crystalvalue", giveNumber());
+        imageCrystal.attr("value", giveNumber());
+        
+        $("#crystals").append(imageCrystal);
+        
+      }
+
   }
   
-  function imageGeneration() {
+  
   for (var i = 0; i < crystalImages.length; i++) {
     
     // For each iteration, we will create an imageCrystal
     var imageCrystal = $("<img>");
-
+    
     // First each crystal will be given the class ".crystal-image".
     // This will allow the CSS to take effect.
     imageCrystal.addClass("crystal-image");
 
     // Each imageCrystal will be given a src link to the crystal image
     imageCrystal.attr("src",crystalImages[i]);
-
+    imageCrystal.attr("name", "idontcare");
     // Each imageCrystal will be given a data attribute called data-crystalValue.
     // This data attribute will be set equal to the array value.
     
     imageCrystal.attr("data-crystalvalue", giveNumber());
+    imageCrystal.attr("value", giveNumber());
     // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
     $("#crystals").append(imageCrystal);
     
   }
-}
-    imageGeneration();
+
+    
   // This time, our click event applies to every single crystal on the page. Not just one.
-  $(".crystal-image").on("click", function() {
+  $(document).on("click", ".crystal-image", function() {
 
     // Determining the crystal's value requires us to extract the value from the data attribute.
     // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
